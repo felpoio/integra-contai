8327d31786480479e0c0e52d380e8ebed9960e51
Felipe Rangel <feliperangel897@gmail.com>
2023-09-27 12:08:50 -0300

    fixed archive name typo

---
+++ calculadora.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from tkinter.ttk import *
+import tkinter as tk
+import tkinter.ttk as ttk
+from PIL import ImageTk, Image
+import webbrowser
+from openpyxl import load_workbook
+
+#carrega a planilha
+caminho = './assets/spreadsheets/LEIAUTE PADRAO.xlsx'
+caminho_icon = "./assets/images/icon-removebg-preview.ico"
+caminho_logo = "./assets/images/LOGO2.png"
+caminho_tema = './assets/themes/azure/azure.tcl'
